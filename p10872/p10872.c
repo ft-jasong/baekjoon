fee5072bf6f27a68381debb945a61b510beace78
jasong <jasong@student.42seoul.kr>
2021-05-22 22:22:27 +0900

    p10872

---
+++ p10872/p10872.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return (1);
+    return (factorial(n - 1) * n);
+}
+
+int main(void)
+{
+    int n;
+
+    scanf("%d", &n);
+    printf("%d", factorial(n));
+    return (0);
+}
