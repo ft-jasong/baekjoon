2a3d58069dabb147ae6a576baeba5b49ca2c4364
jasong <jasong@student.42seoul.kr>
2022-03-20 16:14:47 +0900

    p2577 solved

---
+++ p2577/2577.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+def printNum(num):
+	for i in range(0, 10):
+		print(num[i])
+
+def solution():
+	num = [0] * 10
+	a = int(stdin.readline().rstrip())
+	b = int(stdin.readline().rstrip())
+	c = int(stdin.readline().rstrip())
+	s = str(a * b * c)
+	for i in s:
+		num[int(i)] += 1
+	printNum(num)
+solution()
