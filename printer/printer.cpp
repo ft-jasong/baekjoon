74c28f27f135a689b796b63199f537fd043c6a85
jasong <jasong@student.42seoul.kr>
2022-07-10 20:39:32 +0900

    printer

---
+++ printer/printer.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+	vector<int> v_loc;
+
+	for (int i = 0; i < priorities.size(); i++)
+		v_loc.push_back(i);
+	while (vector::iter)
+	{
+	}
+    return answer;
+}
