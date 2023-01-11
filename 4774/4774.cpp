88e71bad56e56987dab366ba6f315ea6ca78f57d
jasong <jasong@student.42seoul.kr>
2023-01-11 14:50:55 +0900

    4774 failed

--- 4774/4774.cpp
+++ 4774/4774.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <iomanip>
-using namespace std;
 
-int main() {
-	double months[12];
-	double sum = 0;
+int main(void) {
+	std::vector<std::string>	str_v(12);
+	int							_sum;
+	int							remain;
+	int							under_point = 0;
 
-	for(int i = 0; i < 12; i++) {
-		cin >> months[i];
-		sum += months[i];
+	for (int i = 0; i < 12; i++) {
+		std::cin >> str_v[i];
+		for (int j = 0; j < str_v[i].length(); j++) {
+			if (str_v[i][j] == '.') {
+				str_v[i].erase(str_v[i].begin()+ j);
+				break;
+			}
+		}
+		_sum += std::stoi(str_v[i]);
 	}
-
-	double average = sum / 12;
-	double test = 141.4149999999;
-
-	cout << fixed << setprecision(2);
-	cout << "$" << average << endl;
-	return 0;
+	std:: cout << '$' << _sum / 1200 << '.';
+	remain = _sum % 1200;
+	while (under_point < 100) {
+		remain *= 10;
+		under_point = under_point * 10 + remain / 1200;
+		remain = remain % 1200;
+	}
+	if (under_point % 10 > 5)
+		under_point = under_point / 10 + 1;
+	else
+		under_point = under_point / 10;
+	std::cout << under_point;
 }
